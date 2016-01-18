663806ffdf444827dc25cbc66b40f52104f95391
Hans Stevens <hans.stevens@zimmo.be>
2016-01-18 12:30:27 +0100

    Added PingPongBundle with validation

--- src/PingPong/Player/Player.php
+++ src/PingPong/Player/Player.php
@@ -2,25 +2,57 @@
 
 namespace PingPong\Player;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Player
 {
     private $id;
     private $name;
+    private $email;
 
-    private function __construct()
+    public function __construct()
     {
     }
 
-    public static function withName($playerName)
+    public function getId()
     {
-        $player = new Player();
-        $player->name = $playerName;
-
-        return $player;
+        return $this->id;
     }
 
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
     }
 }
