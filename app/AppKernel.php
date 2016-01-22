6809afa2ace22dfd6ccb6aa1fee381d5b82f9938
Murielle Evers <murielle@zimmo.be>
2016-01-22 12:40:16 +0100

    mongodb configuratie

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Dunglas\ApiBundle\DunglasApiBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new PingPongBundle\PingPongBundle(),
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
