9f363a5a27bb295cfc04ebd1faa2234ab35b7255
Koenraad <koenraad@zimmo.be>
2016-01-26 12:30:59 +0100

    Deleting mongo

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Dunglas\ApiBundle\DunglasApiBundle(),
-            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new PingPongBundle\PingPongBundle(),
         ];
 
